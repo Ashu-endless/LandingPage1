a2c6b27b91e81dcef06b78cf5206a8202efa5f4d
Ashu LaptopLenevo <ashu2017ccyadav@gmail.com>
2024-10-27 23:53:04 +0530

    deploytest5

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -13,13 +13,14 @@ const Navbar:FC = () => {
     const [colorChange, setColorchange] = useState(false);
 
     const changeNavbarColor = () => {
+      if(window){
       if (window.scrollY >= 80) {
           setColorchange(true);
       } else {
           setColorchange(false);
-      }
+      }}
   };
-  window && window.addEventListener("scroll", changeNavbarColor);
+  //  window && window.addEventListener("scroll", changeNavbarColor);
     return <> <header style={{zIndex:1}} className={`text-gray-600 body-font fixed top-0 w-full dark:text-gray-400 ${colorChange
     ? "bg-gradient-to-r from-blue-800 to-indigo-900 text-white"
     : ""}`}>
